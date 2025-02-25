ab929a43facafda904fd086c09d8377e84d3017a
Dennis00000 <161707811+Dennis00000@users.noreply.github.com>
2025-02-25 16:10:50 +0200

    Add missing dependency and fix next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverActions: true,
-  }
+  // Remove serverActions since it's now enabled by default
 }
 
 const withNextIntl = require('next-intl/plugin')(
